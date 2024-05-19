99301b60085555981e368e56910e0b0a78ddbd13
AbrahamOliden <abrahamoliden@protonmail.com>
2024-05-18 20:28:18 -0600

    parsed the response and stored it at parsedConnection

--- js/API-connection.js
+++ js/API-connection.js
@@ -1,7 +1,8 @@
 async function itemsList() {
     const connection = await fetch('http://localhost:3001/products');
+    const parsedConnection = connection.json()
 
-    console.log(connection);
+    console.log(parsedConnection);
 }
 
 itemsList();
