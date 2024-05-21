8e3132855835d01a6245b8e48b7f4e185da9320d
AbrahamOliden <abrahamoliden@protonmail.com>
2024-05-20 22:44:32 -0600

    remove whitespace

--- js/API-connection.js
+++ js/API-connection.js
@@ -1,10 +1,25 @@
 async function itemsList() {
     const connection = await fetch('http://localhost:3001/products');
-    const parsedConnection = connection.json()
+    const parsedConnection = connection.json();
 
+    return parsedConnection;
+};
+
+async function createCard(image, name, price) {
+    const connection = await fetch('http://localhost:3001/products', {
+        method: 'POST',
+        headers: {'Content-type': 'application/json'},
+        body: JSON.stringify({
+            image: image,
+            name: name,
+            price: price
+        })
+    });
+
+    const parsedConnection = connection.json();
     return parsedConnection;
 }
 
 export const apiConnection = {
-    itemsList
+    itemsList, createCard
 }
