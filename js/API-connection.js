8c706deb5043d14290843867814885d447dfd11a
AbrahamOliden <abrahamoliden@protonmail.com>
2024-05-21 20:58:27 -0600

    rename sendVideo to sendCard

--- js/API-connection.js
+++ js/API-connection.js
@@ -5,7 +5,7 @@ async function itemsList() {
     return parsedConnection;
 };
 
-async function sendVideo(image, name, price) {
+async function sendCard(image, name, price) {
     const connection = await fetch('http://localhost:3001/products', {
         method: 'POST',
         headers: {'Content-type': 'application/json'},
@@ -21,5 +21,5 @@ async function sendVideo(image, name, price) {
 }
 
 export const apiConnection = {
-    itemsList, sendVideo
+    itemsList, sendCard
 }
