ce1091a65f298b3bda13c78403cfeae977581354
AbrahamOliden <abrahamoliden@protonmail.com>
2024-05-21 22:51:59 -0600

    create async func removeCard with id as parameter

---
+++ js/delete-card.js
@@ -0,0 +1,21 @@
+import { apiConnection } from "./API-connection.js";
+
+document.addEventListener(
+    'DOMContentLoaded',
+    async () => {
+        await apiConnection.itemsList();
+        const deleteButtons = document.querySelectorAll('.delete-button');
+
+        console.log(deleteButtons);
+        async function removeCard(button) {
+            const id = parseInt(button.dataset.id);
+            console.log(id);
+            await apiConnection.deleteCard(id);
+        };
+
+        deleteButtons.forEach((button) => {
+            button.addEventListener('click', () => removeCard(button));
+        });
+    }
+);
+
